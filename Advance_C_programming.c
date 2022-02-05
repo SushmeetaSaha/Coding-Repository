dbbc3b46988c08261331e3e4205e95bfb2e0a5ee
Sushmeeta Saha <sushmeetasaha@gmail.com>
2022-02-05 16:23:12 +0530

    Worked on developer

--- Advance_C_programming.c
+++ Advance_C_programming.c
@@ -1,6 +1,10 @@
 #include<stdio.h>
-
+void greet(char string[],int age){
+    // char *ptr = string;
+    printf("Welcome %s\n",string);
+    printf("Age %d\n",age);
+}
 int main() {
-    printf("This is your shauary ");
+    greet("Shauary",28);
     return 0;
 }
