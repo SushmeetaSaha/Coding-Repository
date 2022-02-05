0385537d6c16063cfc1a8e7b87788ba9d3e77b75
Sushmeeta Saha <sushmeetasaha@gmail.com>
2022-02-05 17:57:57 +0530

    Solved 3 quesions in advance python

--- Advance_C_programming.c
+++ Advance_C_programming.c
@@ -1,15 +1,28 @@
 #include<stdio.h>
-void greet(char string[],int age){
-    // char *ptr = string;
-    printf("Welcome %s\n",string);
-    printf("Age %d\n",age);
+void Question1(){
+    char ptr[20] ;
+    gets(ptr);
+    printf("The string you entered is : %s\n",ptr);
 }
-int sum(int a , int b ){
-    return a + b;
+void Question2(char string[]){
+    int index = 0;
+    int lenght = 0;
+    while(string[index]!='\0'){
+        index++;
+        lenght++;
+    }
+    printf("The lenght of the string %s is %d ",string,lenght);
+}
+void Question3(char string[]){
+    int index = 0;
+    while(string[index]!='\0'){
+        printf(" %c ",string[index]);
+        index++;
+    }
 }
 int main() {
-    greet("Shauary",28);
-    int answer = sum(2,3);
-    printf("The value of answer is %d \n",answer);
-    return 0;
+    // Question1();
+    char name[] = "My name is Shauary ";
+    // Question2(name);
+    Question3(name);
 }
