de13caf2e02a9d90d2e1e85dcef46536c8bd05a4
Sushmeeta Saha <sushmeetasaha@gmail.com>
2022-02-05 16:23:42 +0530

    Worked on the branch developer

--- Advance_C_programming.c
+++ Advance_C_programming.c
@@ -4,7 +4,12 @@ void greet(char string[],int age){
     printf("Welcome %s\n",string);
     printf("Age %d\n",age);
 }
+int sum(int a , int b ){
+    return a + b;
+}
 int main() {
     greet("Shauary",28);
+    int answer = sum(2,3);
+    printf("The value of answer is %d \n",answer);
     return 0;
 }
